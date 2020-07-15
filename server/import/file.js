5d538bc6d8eec788a8e3d334d28daad1d795cc06
Ivan Genesoto <26069066+IvanGenesoto@users.noreply.github.com>
2020-07-15 08:18:52 -0700

    Update dependencies with vulnerabilities. #2

---
+++ server/import/file.js
@@ -0,0 +1,15 @@
+module.exports = function importFile({
+  module, filePath, parentObject, name, makeCamelCase, enumerable
+}) {
+  const value = module.require(filePath)
+  // if (typeof value === 'object' && value.shouldAppendProperties) {
+  //   return Object.entries(value).reduce(appendProperties, parentObject)
+  // }
+  name = makeCamelCase(name)
+  return Object.defineProperty(parentObject, name, {value, enumerable})
+}
+
+// function appendProperties(parentObject, [key, value]) {
+//   if (typeof value === 'object') value = Object.freeze(value)
+//   return Object.defineProperty(parentObject, key, {value, enumerable: true})
+// }
